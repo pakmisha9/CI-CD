a5e8105ce23a15f70140667bc34d638c21a76f10
Misha <pakmisha9@gmail.com>
2024-03-06 22:14:47 +0600

    add

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,7 +3,7 @@ defineProps({ count: Number, times: Number })
 
 const emit = defineEmits(["increment", "clear"])
 const increment = () => {
-	emit("increment", 0)
+	emit("increment", 1)
 }
 const clear = () => {
 	emit("clear")
@@ -14,7 +14,7 @@ const clear = () => {
 	<div class="mb-10 flex flex-col items-center">
 		<div data-test="counter" class="counter text-xl">{{ count }}</div>
 		<div class="flex items-center gap-3">
-			<button data-test="increment" @click.prevent="increment" class="increment mt-4 rounded border bg-gray-100 px-5 py-2 font-semibold">increment +{{ times }}</button>
+			<button data-test="increment" @click.prevent="increment" class="increment mt-4 rounded border bg-gray-100 px-5 py-2 font-semibold">test +{{ times }}</button>
 			<button @click.prevent="clear" class="mt-4 rounded border bg-gray-100 px-5 py-2 font-semibold">Clear</button>
 		</div>
 	</div>
